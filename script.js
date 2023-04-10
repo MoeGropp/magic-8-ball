4473d5f09e7d7f7d5dd4af0d57348db03deedae3
MoeGropp <112837865+MoeGropp@users.noreply.github.com>
2023-04-10 17:30:01 -0500

    Adding JS code

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    var magic8Ball = {};
+    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
+    $("#answer").hide();
+    
+    magic8Ball.askQuestion = function(question){
+        $("#8ball").effect("Shake");
+        $("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png");
+        $("answer").fadeIn(4000);
+       
+        var randomNumber = Math.random();
+        var randomNumberForListOfAnswers = randomNumber *this.listOfAnswers.length;
+        var randomIndex = Math.floor(randomNumberForListOfAnswers);
+        var answer = this.listOfAnswers[randomIndex];
+
+        $("#answer").text(answer);
+        
+    };
+    var onClick = (function(){
+        $("#answer").hide();
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+
+        setTimeout(function(){
+            var question = prompt ("Ask a Yes/No question!");
+            magic8Ball.askQuestion(question);
+        }, 500)
+    };
+
+    $("#questionButton").click(onClick);
+});
